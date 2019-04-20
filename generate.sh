db3f13a819f3ea7674e0b4b902ad9f3baaa83d65
aupasana <aupasana@users.noreply.github.com>
2019-04-20 09:03:13 -0700

    json_to_ical: support well known cities

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./generate_json.py --city $1 --year $2  > data/veda_$1_$2.json
-cat data/veda_$1_$2.json | ./json_to_ical.py > data/veda_$1_$2.ics
+cat data/veda_$1_$2.json | ./json_to_ical.py --city $1 > data/veda_$1_$2.ics
