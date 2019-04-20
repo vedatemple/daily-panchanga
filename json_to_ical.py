e5ce6b56ab81a9f77d88dee23eb08fcff9979ece
aupasana <aupasana@users.noreply.github.com>
2019-04-19 23:11:46 -0700

    minor cleanup

--- json_to_ical.py
+++ json_to_ical.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python
 
+# This file reads stdin for data in the format output by generate_json.py
+# and outputs an ical calendar to stdout
+#
+# See the entries in data/veda_seattle_2019.json for the expected input schema
+
 from ics import Calendar, Event
 from datetime import datetime
 from sys import stdin
@@ -8,49 +13,7 @@ import json
 json_input = stdin.read()
 data = json.loads(json_input)
 
-    # "2019-1-1": {
-    #     "date": "2019-1-1",
-    #     "month": "January",
-    #     "weekday": "Tue",
-    #     "sunrise": "08:03",
-    #     "sunset": "16:22",
-    #     "rahu_start": "14:17",
-    #     "rahu_end": "15:20",
-    #     "yama_start": "10:08",
-    #     "yama_end": "11:10",
-    #     "tithi": [
-    #         {
-    #             "name": "Kṛṣṇa-Ekādaśī",
-    #             "end": "11:58"
-    #         }
-    #     ],
-    #     "nakshatram": [
-    #         {
-    #             "name": "Viśākhā",
-    #             "end": "20:09"
-    #         }
-    #     ],
-    #     "yogam": [
-    #         {
-    #             "name": "Dhṛtiḥ",
-    #             "end": "13:11"
-    #         }
-    #     ],
-    #     "karanam": [
-    #         {
-    #             "name": "Bālavam",
-    #             "end": "11:58"
-    #         },
-    #         {
-    #             "name": "Kaulavam",
-    #             "end": "00:16(+1)"
-    #         }
-    #     ]
-    # },
-
-
-def anga_to_string(item, anga):
-    
+def anga_to_string(item, anga):    
     description_array = []
 
     for v in item[anga]:
@@ -91,7 +54,6 @@ def item_to_name(item):
 
     return name
 
-
 def item_to_ical(item):
     
     ical_event = Event()
@@ -101,7 +63,6 @@ def item_to_ical(item):
     ical_event.uid = 'veda_daily_panchanga_' + item['date']
     ical_event.description = item_to_string(item)
     return ical_event
-
 
 ical = Calendar()
 
